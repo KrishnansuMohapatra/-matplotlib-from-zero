679dd57e10ed3b0c0386e905d8437bc829290887
KrishnansuMohapatra <krishnansumohapatra20@gmail.com>
2026-08-17 19:34:55 +0530

    Add Matplotlib Lesson 1 practice

---
+++ 01_basics/practice.py
@@ -0,0 +1,12 @@
+# 🧠 Practice Challenge
+#
+# 1. Create a list containing the sales for 5 days:
+#    10, 15, 12, 20, 18
+#
+# 2. Create a line plot using the sales data.
+#
+# 3. Display the plot.
+#
+# 4. Try changing the sales values and observe how the graph changes.
+#
+# Try solving these yourself.
