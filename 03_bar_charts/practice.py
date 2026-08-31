de66e7d9fe970b2cf3a93c0c7b5c6c7743ca0aa0
KrishnansuMohapatra <krishnansumohapatra20@gmail.com>
2026-09-01 05:19:26 +0530

    Add completed Matplotlib Lesson 3 practice

---
+++ 03_bar_charts/practice.py
@@ -0,0 +1,30 @@
+# Matplotlib Lesson 3 Practice
+# Bar Charts
+
+import matplotlib.pyplot as plt
+
+# 1. Create a list of five programming languages.
+#
+# Example categories:
+# Python, Java, C++, JavaScript, Go
+
+# 2. Create a list containing the number
+#    of students for each language.
+
+# 3. Create a bar chart using the two lists.
+
+# 4. Add an X-axis label:
+#    "Programming Language"
+
+# 5. Add a Y-axis label:
+#    "Number of Students"
+
+# 6. Add a suitable title.
+
+# 7. Create another bar chart showing
+#    monthly sales for five months.
+
+# 8. Add appropriate labels and a title
+#    to the second chart.
+
+# 9. Display both charts.
